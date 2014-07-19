a1cec6c09eb6b6232733610ea51134187adf8438
skatsuta <katsuta_soshi@cyberagent.co.jp>
2014-07-19 23:57:10 +0900

    Fix a bug

--- spec/spec.sh
+++ spec/spec.sh
@@ -7,10 +7,10 @@ ANSWER="answer.txt"
 
 # 答えファイルがなければ作成
 if [ ! -e $ANSWER ]; then
-  cat $CC | > $ANSWER
+  cat $CC > $ANSWER
 fi
 # 結果ファイルを作成
-mono $EXE | > $RESULT
+mono $EXE > $RESULT
 # diff を取って逆順で表示して色付け
 diff -u $ANSWER $RESULT | gtac | colordiff
 
